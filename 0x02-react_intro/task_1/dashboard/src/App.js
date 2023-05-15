2977091e7783139a66e32ae539fca413b619532b
Eiden Martinez Davila <egabriel0208@proton.me>
2023-05-14 22:23:18 -0400

    task 1 and 2

---
+++ 0x02-react_intro/task_1/dashboard/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import { getFooterCopy, getFullYear } from './utils';
+
+function App() {
+  const date = getFullYear();
+  const footerCopy = getFooterCopy();
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="../public/Holberton_logo.jpg" className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+      </body>
+      <footer className="App-footer"> <p>Copyright {date} - {footerCopy}</p></footer>
+    </div>
+  );
+}
+
+export default App;
