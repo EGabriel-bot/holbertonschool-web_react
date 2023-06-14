b11376b1b9ca6a9c6e25ee83be851275b029b352
Eiden Martinez Davila <egabriel0208@proton.me>
2023-06-14 18:08:42 -0400

    fix task 2 and 3

--- 0x07-React_Immutable/2-nested.js
+++ 0x07-React_Immutable/2-nested.js
@@ -1,11 +1,5 @@
+import { getIn } from 'immutable';
+
 export default function accessImmutableObject(object, array) {
-    Object.keys(object).forEach(key => {
-        if (object[key] instanceof Object) {
-            accessImmutableObject(object[key], array);
-        } else {
-            if (array.includes(key)) {
-                console.log(object[key]);
-            }
-        }
-    });
+  return getIn(object, array);
 }
