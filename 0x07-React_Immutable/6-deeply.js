4407b19728af695f1fc08eaf9f5fa10924672c1e
Eiden Martinez Davila <egabriel0208@proton.me>
2023-06-14 18:12:40 -0400

    fix for task 5 and 6

--- 0x07-React_Immutable/6-deeply.js
+++ 0x07-React_Immutable/6-deeply.js
@@ -1,29 +1,8 @@
 import {Map} from 'immutable';
 
-function mergeDeeplyElements(obj1, obj2) {
-    return Map(obj1.mergeDeep(obj2));
-}
+export default function mergeDeeplyElements(page1	, page2) {
+  const map1 = Map(page1);
+  const map2 = Map(page2);
 
-const page1 = {
-    'user-1': {
-      id: 1,
-      name: 'test',
-      likes: {
-        1: {
-          uid: 1234,
-        }
-      }
-    },
-  };
-  
-  const page2 = {
-    'user-1': {
-      likes: {
-        2: {
-          uid: 134,
-        }
-      }
-    },
-  };
-  
-let obj = mergeDeeplyElements(page1, page2).toJS();
+  return (map1.mergeDeep(map2));
+}
